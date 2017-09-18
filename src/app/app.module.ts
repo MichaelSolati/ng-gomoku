1f46e8ecaef079e410cf01b5bf3eb775e0ec7ac7
Michael Solati <mkslt04@gmail.com>
2017-09-17 20:23:37 -0400

    style: add scss files for components and classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-gomoku' }),
     BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
