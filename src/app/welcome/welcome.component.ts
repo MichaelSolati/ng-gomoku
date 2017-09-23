20451e47bf4b10f61491254b9b1bb7eeadd46864
Michael Solati <mkslt04@gmail.com>
2017-09-22 22:28:51 -0400

    feat(welcome): add 'magic' as well an improved landing page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -9,9 +9,17 @@ import { NavbarService } from '../core/services';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  private _gameTime = 0;
   constructor(private _ns: NavbarService) { }
 
   ngOnInit() {
     this._ns.setTitle('Gomoku');
+  }
+
+  public game(): void {
+    this._gameTime++;
+    if (this._gameTime === 5 && (typeof window !== 'undefined')) {
+      window.open('https://www.youtube.com/watch?v=kYWcV2DIIHU', '_blank');
+    }
   }
 }
